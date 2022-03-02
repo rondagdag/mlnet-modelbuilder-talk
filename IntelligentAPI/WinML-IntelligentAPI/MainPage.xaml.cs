b683effc6291c01aea22cd556317c4bc70ff7e21
Ron Dagdag <rlyle78@gmail.com>
2022-03-02 06:11:16 -0600

    initial

---
+++ IntelligentAPI/WinML-IntelligentAPI/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WinML_IntelligentAPI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+            //Instantiate file picker and store selected file
+            FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".jpg");
+            fileOpenPicker.FileTypeFilter.Add(".png");
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+            StorageFile selectedStorageFile = await fileOpenPicker.PickSingleFileAsync();
+
+
+            //Call ClassifyImage to get image classes
+
+            //Display Image and result
+            ResultsBlock.Text = "Guesses here";
+            await DisplayImage(selectedStorageFile);
+
+        }
+
+        private async Task DisplayImage(StorageFile selectedStorageFile)
+        {
+            SoftwareBitmap softwareBitmap;
+            using (IRandomAccessStream stream = await selectedStorageFile.OpenAsync(FileAccessMode.Read))
+            {
+                // Create the decoder from the stream 
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                // Get the SoftwareBitmap representation of the file in BGRA8 format
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+                softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+            }
+
+            // Display the image
+            SoftwareBitmapSource imageSource = new SoftwareBitmapSource();
+            await imageSource.SetBitmapAsync(softwareBitmap);
+            UIPreviewImage.Source = imageSource;
+        }
+    }
+}
